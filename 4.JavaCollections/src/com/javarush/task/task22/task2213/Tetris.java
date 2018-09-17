2ae6e846c5f52ebb2f9f6bc22108f3476bb98d4e
pluk1 <9225534@gmail.com>
2018-09-17 16:17:36 +0300

    wf

---
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task22.task2213;
+
+public class Tetris {
+
+    private Field field;
+    private Figure figure;
+
+    public static void main(String[] args) {
+
+    }
+}
