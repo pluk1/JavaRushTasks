29206f79b8e2e7379f538a4635d6df8d3f5f627c
pluk55 <pluk@inbox.ru>
2018-09-17 17:45:04 +0300

    done com.javarush.task.task33.task3305

--- 4.JavaCollections/src/com/javarush/task/task33/task3305/Auto.java
+++ 4.JavaCollections/src/com/javarush/task/task33/task3305/Auto.java
@@ -1,5 +1,17 @@
 package com.javarush.task.task33.task3305;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, property="className")
+@JsonSubTypes({
+        @JsonSubTypes.Type(value=Motorbike.class, name="com.javarush.task.task33.task3305.Motorbike"),
+        @JsonSubTypes.Type(value=RaceBike.class, name="com.javarush.task.task33.task3305.RaceBike"),
+        @JsonSubTypes.Type(value=Car.class, name="com.javarush.task.task33.task3305.Car")
+})
+
+
 public abstract class Auto {
     protected String name;
     protected String owner;
--- 4.JavaCollections/src/com/javarush/task/task33/task3305/Parking.java
+++ 4.JavaCollections/src/com/javarush/task/task33/task3305/Parking.java
@@ -1,7 +1,13 @@
 package com.javarush.task.task33.task3305;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
 import java.util.List;
 
+@JsonTypeInfo(use = JsonTypeInfo.Id.MINIMAL_CLASS, include = JsonTypeInfo.As.PROPERTY, property = "className")
+@JsonSubTypes({
+        @JsonSubTypes.Type(value=Parking.class, name=".Parking")})
 public class Parking {
     public String name;
     public String city;
